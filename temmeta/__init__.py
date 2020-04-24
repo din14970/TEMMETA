cb7e716fa73077f48788b1cea7e06c40ef2fc68b
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-04-24 16:20:26 +0200

    begin refactoring

---
+++ temmeta/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+__doc__ = """
+temmeta: transmission electron microscopy (meta)data manipulation
+=================================================================
+"""
+
+# If one does from temmeta import *, it will import api
+__all__ = ["api"]
