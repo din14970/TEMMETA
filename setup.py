859aa79cb75616aed00f5684e04eb4645c2c7e3d
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-04-24 16:58:33 +0200

    minor tweaks for pip packaging

--- setup.py
+++ setup.py
@@ -3,23 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="temmeta",
     version="0.0.1",
-    description="TEMMETA is a library for transmission electron microscopy (TEM) (meta)data manipulation",
+    description=("TEMMETA is a library for transmission electron microscopy "
+                 "(TEM) (meta)data manipulation"),
     url='https://github.com/din14970/TEMMETA',
     author='Niels Cautaerts',
     author_email='nielscautaerts@hotmail.com',
     license='GPL-3.0',
 
-    classifiers=['Development Status :: 5 - Production/Stable',
-                 'Topic :: Scientific/Engineering :: Physics',
-                 'License :: OSI Approved :: BSD License',
+    classifiers=['Topic :: Scientific/Engineering :: Physics',
                  'Intended Audience :: Science/Research',
                  'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7'],
-
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8'],
     keywords='TEM',
     packages=find_packages(exclude=["*tests*", "*examples*"]),
     install_requires=[
